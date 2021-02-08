133dc20b83ef368435db0873e8830ce8167a5203
ismailgo95 <mail.kerja1995@gmail.com>
2021-02-08 08:54:28 +0700

    update data-personal

--- view/data-personal/v_dataPersonal.php
+++ view/data-personal/v_dataPersonal.php
@@ -12,7 +12,8 @@
                 <i class="fas fa-bars"></i>
               </span>
               <h3 class="card-title mt-2 ml-4 text-lg text-bold"> Daftar Anggota</h3>
-              <a href="#" class="float-right btn btn-primary btn-icon-split" data-toggle="modal" data-target="#modal_add" id="btn_tambah">
+              <a href="#" class="float-right btn btn-primary btn-icon-split" data-toggle="modal"
+                data-target="#modal-default" id="btn_tambah">
                 <span class="icon text-white">
                   <i class="fas fa-plus-square"></i>
                 </span>
@@ -42,7 +43,10 @@
                     <td>Muhammad ismail</td>
                     <td>Muhammad ismail</td>
                     <td>Muhammad ismail</td>
-                    <td>Muhammad ismail</td>
+                    <td>
+                      <a class="btn btn-warning" href="#"><i class="fas fa-pencil-alt"></i></a>
+                      <a class="btn btn-danger" href="#"><i class="fas fa-trash"></i></a>
+                    </td>
                   </tr>
                 </tbody>
               </table>
@@ -60,4 +64,43 @@
     </div><!-- /.container-fluid -->
   </section>
   <!-- /.content -->
+</div>
+
+
+<!-- Modal -->
+<div class="modal fade" id="modal-default">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h4 class="modal-title">Tambah Anggota</h4>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <form>
+        <div class="modal-body">
+          <div class="form-group">
+            <label for="nama">Nama</label>
+            <input type="text" class="form-control" id="nama" placeholder="Masukan Nama">
+          </div>
+          <div class="form-group">
+            <label for="nip">NIP</label>
+            <input type="text" class="form-control" id="nip" placeholder="Masukan Nip">
+          </div>
+          <div class="form-group">
+            <label for="jabatan">Jabatan</label>
+            <input type="text" class="form-control" id="jabatan" placeholder="Masukan Jabatan">
+          </div>
+          <div class="form-group">
+            <label for="keterangan">Keterangan</label>
+            <input type="text" class="form-control" id="keterangan" placeholder="Masukan Keterangan">
+          </div>
+        </div>
+        <div class="modal-footer justify-content-between">
+          <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+          <button type="button" class="btn btn-primary">Save changes</button>
+        </div>
+      </form>
+    </div>
+  </div>
 </div>
